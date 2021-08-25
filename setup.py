141f057b458ad8e00d0f80fe5d1b1afe6032c57a
Lukas <89483152+LukasVrox@users.noreply.github.com>
2021-08-25 17:39:48 +0200

    Updated setup.py
    Added AASCII-Art on Start.
    Added some safety from bad input.
    Overall made the script better.

--- setup.py
+++ setup.py
@@ -4,16 +4,48 @@ import time
 import getpass
 from os import path
 
+print ("""
+  ____        _     _               
+ |  _ \ _   _| |__ | |__   ___ _ __ 
+ | |_) | | | | '_ \| '_ \ / _ \ '__|
+ |  _ <| |_| | |_) | |_) |  __/ |   
+ |_|_\_\\__,_|_.__/|_.__/ \___|_|   
+ |  _ \ _   _  ___| | ___   _       
+ | | | | | | |/ __| |/ / | | |      
+ | |_| | |_| | (__|   <| |_| |      
+ |____/ \__,_|\___|_|\_\\__, |      
+ |  _ \(_) ___ ___      |___/       
+ | |_) | |/ __/ _ \                 
+ |  __/| | (_| (_) |                
+ |_|   |_|\___\___/ 
 
-input = input('Please enter your Drive letter of the pico: ')
+
+ Reset Pico before use!! 
+ Press big white Button on startup!
+""")
+
+input = input('Please enter the Drive letter of the Raspberry Pico: ')
+length = len(input)
+typee = type(length)
+check1 = ':'
+
+
+if ':' in input:
+    print('Please type the letter without ":"')
+    exit()
+elif length > 2 :
+    print('Thats not possible')
+    exit()
+
 print('You selected ' + input)
-checkpath = path.exists('F:/lib')
-username = getpass.getuser()
-src = 'C:/Users/' + username + '/Desktop/Python/DuckyPySetup/CircuitPython.uf2'
-src1 = 'C:/Users/' + username + '/Desktop/Python/DuckyPySetup/Bundle/lib/adafruit_hid'
-src2 = 'C:/Users/' + username + '/Desktop/Python/DuckyPySetup/pico-ducky-main/duckyinpython.py'
-src3 = 'C:/Users/' + username + '/Desktop/Python/DuckyPySetup/flash_nuke.uf2'
-#print(src)
+
+checkpath = path.exists(input + ':/lib')
+
+src = 'CircuitPython.uf2'
+src1 = 'Bundle/lib/adafruit_hid'
+src2 = 'pico-ducky-main/duckyinpython.py'
+src3 = 'flash_nuke.uf2'
+
 dst =  input + ':/'
 dst1 = input + ':/lib/adafruit_hid'
 
@@ -24,7 +56,7 @@ def inst():
     print('Copy adafruit_hid to /lib on the Pi')
     shutil.copytree(src1, dst1)
     time.sleep(3)
-    print('Copy duckyinpython to' + input)
+    print('Copy duckyinpython to ' + input)
     shutil.copy(src2, dst)
     time.sleep(3)
     print('remove code.py')
@@ -32,16 +64,24 @@ def inst():
     time.sleep(3)
     print('Rename duckyinpython to code.py')
     os.rename(input + ':/duckyinpython.py', input + ':/code.py' )
+    check_lib = path.exists(input + ':/lib/adafruit_hid')
+    if check_lib == True:
+        print('Setup is complete')
+        exit()
+    else:
+        print('Something went wrong with the setup. Please try it again')
 
 def start():
     if checkpath == False:
-        print('NUKING THE PI!!!')
+        time.sleep(1)
+        print('Flashing Py to factory defaults...')
         shutil.copy(src3, dst)
-        print('Wait 10 for pi reboot')
+        print('Wait 10sec for the pi to reboot..')
         time.sleep(10)
         inst()
     elif checkpath == True:
-        print('Pi is installed, please reset')
+        time.sleep(1)
+        print('Pi is installed, please reset..')
 
 start()
 
